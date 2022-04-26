73d6f4b86ff03b0f1a9b5bf4af3ee79e729f0dc7
mhdabdellah <86131274+mhdabdellah@users.noreply.github.com>
2022-04-26 12:25:34 +0200

    commit

--- image/admin.py
+++ image/admin.py
@@ -3,5 +3,12 @@ from django.contrib import admin
 from image.models import Image
 
 # Register your models here.
+class  ImageAdmin(admin.ModelAdmin):
+    fields = ['img','type','description','patient']
+    list_display = ['img','type','description','technicien','patient']
+    list_display_links = ['technicien']
+    list_editable = ['img','type','description','patient']
+    list_filter = ['type','description','technicien','patient']
+    search_fields = ['type','description','technicien','patient']
 
-admin.site.register(Image)
+admin.site.register(Image,ImageAdmin)
