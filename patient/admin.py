e4344e3cbccab5c15f02cdec080e989d41494e1b
mhdabdellah <86131274+mhdabdellah@users.noreply.github.com>
2022-04-26 12:06:03 +0200

    customise admin panel

--- patient/admin.py
+++ patient/admin.py
@@ -4,4 +4,15 @@ from patient.models import Patient
 
 # Register your models here.
 
-admin.site.register(Patient)
+admin.site.site_header = "Image medical Admin Panel"
+admin.site.site_title = "Image medical Admin Panel"
+
+class  PatientAdmin(admin.ModelAdmin):
+    fields = ['nom','prenom','sexe','dateNaissance','lieuNaissance','lieuResidance','NumTel1','NumTel2','Assurance']
+    list_display = ['secretaire','nom','prenom','sexe','dateNaissance','lieuNaissance','lieuResidance','NumTel1','NumTel2','Assurance']
+    list_display_links = ['secretaire']
+    list_editable = ['nom','prenom','sexe','dateNaissance','lieuNaissance','lieuResidance','NumTel1','NumTel2','Assurance']
+    list_filter = ['nom','prenom','sexe','dateNaissance','lieuNaissance','lieuResidance','NumTel1','NumTel2']
+    search_fields = ['nom','prenom','sexe','dateNaissance','lieuNaissance','lieuResidance','NumTel1','NumTel2']
+
+admin.site.register(Patient,PatientAdmin)
--- patient/apps.py
+++ patient/apps.py
@@ -1,5 +1,11 @@
 from django.apps import AppConfig
+from suit.apps import DjangoSuitConfig
 
 
 class PatientConfig(AppConfig):
     name = 'patient'
+
+class SuiteConfig(DjangoSuitConfig):
+    # layout = "horizontal" 
+    # vous pouvez utiliser 
+    layout = "vertical"
