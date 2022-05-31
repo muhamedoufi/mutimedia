fa84c4e41ef49a10a22e4848ca9fb70b969898ef
muhamedoufi <muhamedoufi@gmail.com>
2022-05-31 19:13:26 +0000

    django admin group list_display

--- patient/admin.py
+++ patient/admin.py
@@ -1,9 +1,37 @@
 from django.contrib import admin
 
 from patient.models import Patient
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
+from django.contrib.auth.models import User
 
+# class usr_fields(admin.ModelAdmin):
+#     class Meta:
+#         model = User
+
+#     list_display = ('get_groups')
+
+#     def get_groups(self, obj):
+#         return obj.groups.values_list('name',flat=True)
+# # Re-register UserAdmin
+#     get_groups.short_description = 'Groups'
+
+class UserAdminWithGroup(UserAdmin):
+    def get_groups(self, obj):
+        queryset = obj.groups.values_list('name',flat=True)
+        groups = []
+        for group in queryset:
+            groups.append(group)
+        
+        return ' '.join(groups)
+
+    list_display = UserAdmin.list_display + ('get_groups',)
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdminWithGroup)
+# admin.site.unregister(User)
+# admin.site.register(User, usr_fields)
 admin.site.site_header = "Image medical Admin Panel"
 admin.site.site_title = "Image medical Admin Panel"
 
